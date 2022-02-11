d94892334d6e0507314509f0e81b9e024d237ca8
Erick Garcia <erickjg1001@gmail.com>
2022-02-11 14:29:44 -0700

    Finished mult.asm

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,29 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@2
+M = 0   // Reset value in RAM[2]
+@1
+D = M   // Load value from RAM[1] into D
+@3
+M = D   // Put value from RAM[1] into RAM[3] to serve as a loop counter
+@1
+D = M
+@SKIP_LOOP
+D;JEQ
+
+(MULT_LOOP)
+    @0
+    D = M       // Load value from RAM[0]
+    @2
+    M = M + D   // Add value from RAM[0] into RAM[2]
+    @3
+    M = M - 1   // Decrement counter
+    D = M
+    // If counter has not reached 0, loop
+    @MULT_LOOP
+    D;JNE
+(SKIP_LOOP)
+(END)
+    @END
+    0;JMP
