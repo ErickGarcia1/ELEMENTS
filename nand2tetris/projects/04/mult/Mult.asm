3bc64521caeff0ba67fb30faddbbec792fe3847b
Erick <erickjg1001@gmail.com>
2022-02-11 20:16:03 -0700

    A little bit of work

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,28 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// @1
+// D = A   //Load 1 into D
+// @16
+// M = D   //Load 1 into RAM[16]
+// @2
+// D = M   //Load RAM[2] into D
+// @17
+// M = D   //Load contents of RAM[2] into RAM[17]
+// @16
+// D = M   //Load bitmask into D
+// M = M + 1
+
+@1
+D = M
+@2
+M = D
+@0
+D = M
+@2
+M = D + M
+
+(END)
+    @END
+    0;JMP
